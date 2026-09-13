8648e8383ae34f914f90a50531586be1eefb50c6
jiuqianqi <jiuqianqi97@gmail.com>
2026-09-13 13:35:48 +0800

    refactor: use usize for coordinates and document edge cases

--- src/editor.rs
+++ src/editor.rs
@@ -64,8 +64,11 @@ impl Editor {
 
     fn draw_welcome_message() -> Result<()> {
         let welcome_message = format!("{NAME} editor -- version {VERSION}");
-        let width = Terminal::size()?.width as usize;
+        let width = Terminal::size()?.width;
         let len = welcome_message.len();
+
+        // We allow this since we don't care if our welcome message is put _exactly_ in the middle.
+        // It's allowed to be a bit to the left or right.
         let padding = width.saturating_sub(len) / 2;
         let space = " ".repeat(padding.saturating_sub(1));
         let mut welcome_message = format!("~{space}{welcome_message}");
@@ -81,6 +84,9 @@ impl Editor {
         let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
+
+            // We allow this since we don't care if our welcome message is put _exactly_ in the middle.
+            // It's allowed to be a bit up or down
             if current_row == height / 3 {
                 Self::draw_welcome_message()?;
             } else {
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -8,16 +8,22 @@ use crossterm::{
 
 #[derive(Clone, Copy, Default)]
 pub struct Position {
-    pub x: u16,
-    pub y: u16,
+    pub x: usize,
+    pub y: usize,
 }
 
 #[derive(Clone, Copy, Default)]
 pub struct Size {
-    pub width: u16,
-    pub height: u16,
+    pub width: usize,
+    pub height: usize,
 }
 
+/// Represents the Terminal.
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the cursor out of these bounds, it will also be truncated.
 pub struct Terminal;
 
 impl Terminal {
@@ -41,8 +47,11 @@ impl Terminal {
         Self::queue_command(Clear(ClearType::CurrentLine))
     }
 
+    /// Moves the cursor to the given Position.
+    /// # Arguments
+    /// * `Position` - the  `Position`to move the cursor to. Will be truncated to `u16::MAX` if bigger.
     pub fn move_cursor_to(position: Position) -> Result<()> {
-        Self::queue_command(cursor::MoveTo(position.x, position.y))
+        Self::queue_command(cursor::MoveTo(position.x as u16, position.y as u16))
     }
 
     pub fn hide_cursor() -> Result<()> {
@@ -57,9 +66,15 @@ impl Terminal {
         Self::queue_command(Print(string))
     }
 
+    /// Returns the current size of this Terminal.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
     pub fn size() -> Result<Size> {
         let (width, height) = terminal::size()?;
-        Ok(Size { width, height })
+        Ok(Size {
+            width: width as usize,
+            height: height as usize,
+        })
     }
 
     pub fn execute() -> Result<()> {
