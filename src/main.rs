a2b9dfa2fc7cc5d9445e6574c4dd6fa9aa128490
jiuqianqi <jiuqianqi97@gmail.com>
2026-09-13 10:14:34 +0800

    feat: enable raw mode with crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::io::{self, Read};
 
+use crossterm::terminal;
+
 fn main() {
+    terminal::enable_raw_mode().unwrap();
     for b in io::BufReader::new(io::stdin()).bytes() {
         let c = b.unwrap() as char;
-        println!("{c}");
+        println!("{c}\r");
         if c == 'q' {
+            terminal::disable_raw_mode().unwrap();
             break;
         }
     }
