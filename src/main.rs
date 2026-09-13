de6f282e45168b1dd753a0089d1c5fae64941264
jiuqianqi <jiuqianqi97@gmail.com>
2026-09-13 10:22:36 +0800

    feat: add error handling for raw mode

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ use crossterm::terminal;
 fn main() {
     terminal::enable_raw_mode().unwrap();
     for b in io::BufReader::new(io::stdin()).bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
-        if c == 'q' {
-            terminal::disable_raw_mode().unwrap();
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => eprintln!("Error: {err}\r"),
         }
     }
+    terminal::disable_raw_mode().unwrap();
 }
