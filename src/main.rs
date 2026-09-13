e3c8b1d5d52f6aed307c33c8de490ea4b9509047
jiuqianqi <jiuqianqi97@gmail.com>
2026-09-13 10:11:34 +0800

    feat: read keypresses from stdin and exit on 'q'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    for b in io::BufReader::new(io::stdin()).bytes() {
+        let c = b.unwrap() as char;
+        println!("{c}");
+        if c == 'q' {
+            break;
+        }
+    }
 }
